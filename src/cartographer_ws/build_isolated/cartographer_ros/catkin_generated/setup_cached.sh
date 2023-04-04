e83b4b54ed8df4ed12c7fd85388f094999793ebd
ChaoXi845 <50503844+ChaoXi845@users.noreply.github.com>
2023-04-04 19:12:09 +0800

    Add files via upload

---
+++ src/cartographer_ws/build_isolated/cartographer_ros/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tide/catkin_ws/devel_isolated/cartographer_ros:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/tide/catkin_ws/install_isolated/lib:/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/home/tide/catkin_ws/install_isolated/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
+export ROS_PACKAGE_PATH="/home/tide/catkin_ws/src/cartographer_ros/cartographer_ros:/home/tide/catkin_ws/install_isolated/share:$ROS_PACKAGE_PATH"
